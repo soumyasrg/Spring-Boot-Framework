70afd213841a101e230aa350f6c891edbcb25e06
Soumya Ranjan <somuranj@gmail.com>
2023-02-05 19:41:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Boot along with Spring Data JPA, Data REST, Profiles, Logging, Health Checks, Spring Security,DB Caching, Spring Batch, Unit testing using MockMVC, Spring JMS, Swagger Documentation, Spring Validations, REST File upload
+
---
+++ productrestapi/main/springweb/repos/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.soumya.springweb.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bharath.springweb.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
